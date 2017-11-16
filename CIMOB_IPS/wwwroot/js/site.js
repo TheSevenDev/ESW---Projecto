031fe3ca58132c99cf909c3508ebe07ccebb18a3
BrunoMPereira <brunop.esac@hotmail.com>
2017-11-16 17:16:48 +0000

    Notification and Settings icon

--- CIMOB_IPS/wwwroot/js/site.js
+++ CIMOB_IPS/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿// Write your JavaScript code.
+
+function toogleSettings()
+{   
+    document.getElementById("submenu-notifications").style.display = "none";
+    var settingsMenu = document.getElementById("submenu-settings");
+    settingsMenu.style.display === "none" ? settingsMenu.style.display = "block" : settingsMenu.style.display = "none";
+
+
+}
+
+
+function toogleNotifications()
+{
+    document.getElementById("submenu-settings").style.display = "none";
+    var notificationMenu = document.getElementById("submenu-notifications");
+    notificationMenu.style.display === "none" ? notificationMenu.style.display = "block" : notificationMenu.style.display = "none";
+}
