177ffc918c9f24e8a68449cfa22b45b66f0d7ba4
BrunoPereira <brunop.esac@hotmail.com>
2017-11-17 01:14:30 +0000

    Adjusted colors. Help button and screen added.

--- CIMOB_IPS/wwwroot/js/site.js
+++ CIMOB_IPS/wwwroot/js/site.js
@@ -9,10 +9,27 @@ function toogleSettings()
 
 }
 
-
 function toogleNotifications()
 {
     document.getElementById("submenu-settings").style.display = "none";
     var notificationMenu = document.getElementById("submenu-notifications");
     notificationMenu.style.display === "none" ? notificationMenu.style.display = "block" : notificationMenu.style.display = "none";
 }
+
+function toogleHelp() {
+    //hideMenus();
+    var fade = document.getElementById("fade-background");
+    var help = document.getElementById("help-content");
+    var closeHelp = document.getElementById("help-close");
+
+    fade.style.display === "none" ? fade.style.display = "block" : fade.style.display = "none";
+    help.style.display === "none" ? help.style.display = "block" : help.style.display = "none";
+    closeHelp.style.display === "none" ? closeHelp.style.display = "block" : closeHelp.style.display = "none";
+}
+
+function hideMenus()
+{
+    document.getElementById("submenu-settings").style.display = "none";
+    document.getElementById("submenu-notifications").style.display = "none";
+}
+
