8ef9a7c84988c4e0de6e5587ae9fa04ee66265ef
Marcos <marcosletras97@gmail.com>
2017-11-23 21:53:06 +0000

    login layout

--- CIMOB_IPS/Controllers/UserController.cs
+++ CIMOB_IPS/Controllers/UserController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace CIMOB_IPS.Controllers
 {
@@ -17,8 +20,9 @@ namespace CIMOB_IPS.Controllers
 
         public IActionResult Login()
         {
-
+            
             return View();
+           
         }
 
         [ActionName("ConvidarTecnico")]
@@ -27,5 +31,14 @@ namespace CIMOB_IPS.Controllers
             return View("Invite");
         }
 
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    
     }
 }
