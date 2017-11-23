a028b261f6ec0652a9c1bac57ab52cfdd310314f
BrunoMPereira <brunop.esac@hotmail.com>
2017-11-23 23:01:38 +0000

    Convidar técnico

--- CIMOB_IPS/Controllers/UserController.cs
+++ CIMOB_IPS/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace CIMOB_IPS.Controllers
 {
@@ -25,6 +26,24 @@ namespace CIMOB_IPS.Controllers
         public IActionResult Invite()
         {
             return View("Invite");
+        }
+
+        public IActionResult InviteTec(IFormCollection form)
+        {
+            string destination = Convert.ToString(form["email"]);
+            SendEmailToTec(destination);
+
+            return View("Invite");
+        }
+
+        private void SendEmailToTec(string emailTec)
+        {
+            string subject = "Convite para registo no CIMOB-IPS";
+
+            string body = "Olá, <br> Recebeu um convite para se registar na aplicação do CIMOB-IPS.<br> " +
+                "Clique <a href=\"www.google.pt\">aqui</a> para confirmar";
+
+            Email.SendEmail(emailTec, subject, body);
         }
 
     }
