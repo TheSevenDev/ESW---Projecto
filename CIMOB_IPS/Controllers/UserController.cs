3e5893116752944d230ab0cbbf1cddcb60cdc541
Marcos <marcosletras97@gmail.com>
2017-11-24 01:29:43 +0000

    Revert "UserController"
    This reverts commit 85e6302b04fc2f831f2063e3af121822b7237878.

--- CIMOB_IPS/Controllers/UserController.cs
+++ CIMOB_IPS/Controllers/UserController.cs
@@ -6,10 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
-<<<<<<< HEAD
 using CIMOB_IPS.Models;
-=======
->>>>>>> 0f3cf9c2935a87209e9057cd4c593c9dad49685f
 using Microsoft.AspNetCore.Http;
 
 namespace CIMOB_IPS.Controllers
@@ -35,30 +32,10 @@ namespace CIMOB_IPS.Controllers
             return View("Login");
         }
 
-
-
         [ActionName("ConvidarTecnico")]
         public IActionResult Invite()
         {
             return View("Invite");
-        }
-
-        public IActionResult InviteTec(IFormCollection form)
-        {
-            string destination = Convert.ToString(form["email"]);
-            SendEmailToTec(destination);
-
-            return View("Invite");
-        }
-
-        private void SendEmailToTec(string emailTec)
-        {
-            string subject = "Convite para registo no CIMOB-IPS";
-
-            string body = "Olá, <br> Recebeu um convite para se registar na aplicação do CIMOB-IPS.<br> " +
-                "Clique <a href=\"www.google.pt\">aqui</a> para confirmar";
-
-            Email.SendEmail(emailTec, subject, body);
         }
 
 
