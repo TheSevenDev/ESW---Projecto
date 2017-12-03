8f93528804437db54db409b9f6e7ddfe957da13e
Marcos <marcosletras97@gmail.com>
2017-12-03 22:31:10 +0000

    ChangePW2

--- CIMOB_IPS/Controllers/UserController.cs
+++ CIMOB_IPS/Controllers/UserController.cs
@@ -10,6 +10,8 @@ using CIMOB_IPS.Models;
 using System.Data;
 using System.Security.Cryptography;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CIMOB_IPS.Controllers
 {
@@ -371,114 +373,50 @@ namespace CIMOB_IPS.Controllers
         }
 
 
-        public IActionResult UpdatePassword()
+        public int GetCurrentUserID()
         {
-           
-
-            if (!User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-
-
-
-
-            return View();
+            return int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
         }
 
         [HttpPost]
-        public IActionResult UpdatePasswordAux(UpdatePasswordViewModel model)
+        public IActionResult UpdatePassword([Bind("Password")] Account account, UpdatePasswordViewModel model)
         {
-            Account account = GetAccountModelByID(1);
-
-            string userPW = Convert.ToString(account.Password);
-            string actualPW = Convert.ToString(model.Account.Password);
+            
+            string confirmation = Convert.ToString(model.Confirmation);
             string newPW = Convert.ToString(model.NewPassword);
-            string confPW = Convert.ToString(model.Confirmation);
 
-
-            if (userPW.Equals(actualPW))
+            if (GetCurrentUserID() != account.IdAccount)
             {
-                if (model.NewPassword.Equals(model.Confirmation))
-                {
-                    ChangePassword(account.Email, model.NewPassword);
-                }
+                return BadRequest();
             }
-            return View("UpdatePassword");
 
+
+            //não está a apresentar erros na pagina
+            if (confirmation.Equals(newPW))
+            {
+
+
+                using (SqlConnection sqlConnection = new SqlConnection(CIMOB_IPS_DBContext.ConnectionString))
+                {
+                    //create await  
+
+                    using (SqlCommand command = sqlConnection.CreateCommand())
+                    {
+                        command.CommandText = "UPDATE Account SET password = @Password WHERE id_account = @IdAccount";
+                        command.Parameters.AddWithValue("@Password", account.Password);
+                        sqlConnection.Open();
+                        command.ExecuteNonQuery();
+                        sqlConnection.Close();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("UpdatePassword");
         }
 
 
-        public Account GetAccountModelByID(int id)
-        {
-            var account = new Account { IdAccount = id };
 
-            using (SqlConnection sqlConnection = new SqlConnection(CIMOB_IPS_DBContext.ConnectionString))
-            {
-                sqlConnection.Open();
-                string sqlQueryStringStudent = "SELECT id_student, a.email, c.name, s.name, address, cc, telephone, " +
-                    "n.description, credits, student_num FROM Student s, Course c, Nationality n, Account a " +
-                    "WHERE s.id_account = @Id AND s.id_course = c.id_course AND s.id_nationality = n.id_nationality " +
-                    "AND s.id_account = a.id_account";
-
-                SqlCommand commandStudent = new SqlCommand(sqlQueryStringStudent, sqlConnection);
-                commandStudent.Parameters.AddWithValue("@Id", id);
-                SqlDataReader reader = commandStudent.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var modelStudent = new Student
-                    {
-                        IdStudent = reader.GetInt64(0),
-                        IdCourseNavigation = new Course { Name = reader.GetString(2) },
-                        Name = reader.GetString(3),
-                        Address = reader.GetString(4),
-                        Cc = reader.GetInt64(5),
-                        Telephone = reader.GetInt64(6),
-                        IdNationalityNavigation = new Nationality { Description = reader.GetString(7) },
-                        Credits = reader.GetInt32(8),
-                        StudentNum = reader.GetInt64(9)
-                    };
-
-                    account.Email = reader.GetString(1);
-
-                    reader.Close();
-
-                    //VER SE HA MELHOR FORMA PA FAZER ISTO
-                    account.Student.Add(modelStudent);
-
-                    return account;
-                }
-
-                reader.Close();
-
-                SqlCommand commandtechnician = new SqlCommand("select t.*, a.email from Technician t, Account a " +
-                    "where t.id_account=@Id and a.id_account = t.id_account", sqlConnection);
-                commandtechnician.Parameters.AddWithValue("@Id", id);
-                SqlDataReader reader2 = commandtechnician.ExecuteReader();
-
-                while (reader2.Read())
-                {
-                    var modelTech = new Technician
-                    {
-                        IdTechnician = reader2.GetInt64(0),
-                        Name = reader2.GetString(2),
-                        Telephone = reader2.GetInt64(3),
-                        IsAdmin = reader2.GetBoolean(4)
-                    };
-
-                    account.Email = reader2.GetString(5);
-
-                    reader2.Close();
-
-                    account.Technician.Add(modelTech);
-
-                    return account;
-                }
-            }
-
-            return null;
-        }
 
         private void SendEmailToStudent(String emailStudent)
         {
