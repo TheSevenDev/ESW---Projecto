2caf750d8f9e49fc24618d2f8081aff4ca6aed2a
Marcos <marcosletras97@gmail.com>
2017-12-03 20:46:24 +0000

    ChangePW

--- CIMOB_IPS/Controllers/UserController.cs
+++ CIMOB_IPS/Controllers/UserController.cs
@@ -14,7 +14,7 @@ using System.Text;
 namespace CIMOB_IPS.Controllers
 {
     public class UserController : Controller
-    {   
+    {
         private const int NEW_PW_MAX_LENGTH = 8;
 
         public IActionResult Register()
@@ -325,7 +325,8 @@ namespace CIMOB_IPS.Controllers
         }
 
 
-        private void ChangePassword(string _email, String _newpassword) {
+        private void ChangePassword(string _email, String _newpassword)
+        {
 
             using (SqlConnection connection = new SqlConnection(CIMOB_IPS_DBContext.ConnectionString))
             using (SqlCommand command = new SqlCommand("", connection))
@@ -367,7 +368,117 @@ namespace CIMOB_IPS.Controllers
 
         }
 
-     
+
+        public IActionResult UpdatePassword()
+        {
+           
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePasswordAux(UpdatePasswordViewModel model)
+        {
+            Account account = GetAccountModelByID(1);
+
+            string userPW = Convert.ToString(account.Password);
+            string actualPW = Convert.ToString(model.Account.Password);
+            string newPW = Convert.ToString(model.NewPassword);
+            string confPW = Convert.ToString(model.Confirmation);
+
+
+            if (userPW.Equals(actualPW))
+            {
+                if (model.NewPassword.Equals(model.Confirmation))
+                {
+                    ChangePassword(account.Email, model.NewPassword);
+                }
+            }
+            return View("UpdatePassword");
+
+        }
+
+
+        public Account GetAccountModelByID(int id)
+        {
+            var account = new Account { IdAccount = id };
+
+            using (SqlConnection sqlConnection = new SqlConnection(CIMOB_IPS_DBContext.ConnectionString))
+            {
+                sqlConnection.Open();
+                string sqlQueryStringStudent = "SELECT id_student, a.email, c.name, s.name, address, cc, telephone, " +
+                    "n.description, credits, student_num FROM Student s, Course c, Nationality n, Account a " +
+                    "WHERE s.id_account = @Id AND s.id_course = c.id_course AND s.id_nationality = n.id_nationality " +
+                    "AND s.id_account = a.id_account";
+
+                SqlCommand commandStudent = new SqlCommand(sqlQueryStringStudent, sqlConnection);
+                commandStudent.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = commandStudent.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var modelStudent = new Student
+                    {
+                        IdStudent = reader.GetInt64(0),
+                        IdCourseNavigation = new Course { Name = reader.GetString(2) },
+                        Name = reader.GetString(3),
+                        Address = reader.GetString(4),
+                        Cc = reader.GetInt64(5),
+                        Telephone = reader.GetInt64(6),
+                        IdNationalityNavigation = new Nationality { Description = reader.GetString(7) },
+                        Credits = reader.GetInt32(8),
+                        StudentNum = reader.GetInt64(9)
+                    };
+
+                    account.Email = reader.GetString(1);
+
+                    reader.Close();
+
+                    //VER SE HA MELHOR FORMA PA FAZER ISTO
+                    account.Student.Add(modelStudent);
+
+                    return account;
+                }
+
+                reader.Close();
+
+                SqlCommand commandtechnician = new SqlCommand("select t.*, a.email from Technician t, Account a " +
+                    "where t.id_account=@Id and a.id_account = t.id_account", sqlConnection);
+                commandtechnician.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader2 = commandtechnician.ExecuteReader();
+
+                while (reader2.Read())
+                {
+                    var modelTech = new Technician
+                    {
+                        IdTechnician = reader2.GetInt64(0),
+                        Name = reader2.GetString(2),
+                        Telephone = reader2.GetInt64(3),
+                        IsAdmin = reader2.GetBoolean(4)
+                    };
+
+                    account.Email = reader2.GetString(5);
+
+                    reader2.Close();
+
+                    account.Technician.Add(modelTech);
+
+                    return account;
+                }
+            }
+
+            return null;
+        }
+
+
 
     }
-}
+    }
---
+++ CIMOB_IPS/Models/UpdatePasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIMOB_IPS.Models
+{
+    public class UpdatePasswordViewModel
+    {
+        public Account Account { get; set; }
+        public string NewPassword { get; set; }
+        public string Confirmation { get; set; }
+    }
+}
