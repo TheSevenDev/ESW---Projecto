2a2a5969cbe424100630a12a9e1b70537e1f6b2b
Diogo Abreu <diogo_oliveira_9@hotmail.com>
2017-12-27 17:01:16 +0000

    started rf12

--- CIMOB_IPS/Controllers/HomeController.cs
+++ CIMOB_IPS/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CIMOB_IPS.Models;
+using Microsoft.EntityFrameworkCore;
+using CIMOB_IPS.Models.ViewModels;
+using System.Threading.Tasks;
 
 namespace CIMOB_IPS.Controllers
 {
@@ -9,6 +12,22 @@ namespace CIMOB_IPS.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Contact()
+        {
+            using (var context = new CIMOB_IPS_DBContext(new DbContextOptions<CIMOB_IPS_DBContext>()))
+            {
+                var coordenators = await context.Coordenator
+                    .Include(c => c.IdCourseNavigation)
+                    .ToListAsync();
+
+                var technicians = await context.Technician
+                    .Include(t => t.IdAccountNavigation)
+                    .ToListAsync();
+
+                return View(new ContactsViewModel { Coordenators = coordenators, Technicians = technicians });
+            }
         }
     }
 }
---
+++ CIMOB_IPS/Models/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIMOB_IPS.Models.ViewModels
+{
+    public class ContactsViewModel
+    {
+        public List<Coordenator> Coordenators { get; set; }
+        public List<Technician> Technicians { get; set; }
+    }
+}
