a868c6930598035e994f671a4a35c1ef9d9c9194
TheSevenDev <diogo_oliveira_9@hotmail.com>
2018-01-09 00:04:31 +0000

    added wpf project

--- CIMOB_IPS_BackOffice/MainWindow.xaml.cs
+++ CIMOB_IPS_BackOffice/MainWindow.xaml.cs
@@ -23,6 +23,27 @@ namespace CIMOB_IPS_BackOffice
         public MainWindow()
         {
             InitializeComponent();
+            //DisplayVersion();
         }
-    }
+
+        /*
+        private void DisplayVersion()
+        {
+            var version = string.Format("assembly: {0}", ((System.Reflection.AssemblyFileVersionAttribute)(System.Reflection.Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(System.Reflection.AssemblyFileVersionAttribute),false)[0])).Version);
+
+            if (System.DeploymentApplicationDeployment.IsNetworkDeployed)
+            {
+
+               System.Deployment.Application.ApplicationDeployment ad = System.Deployment.Application.ApplicationDeployment.CurrentDeployment;
+               version = string.Format("ClickOnce: {0}", ad.CurrentVersion.ToString());
+
+            }
+
+             Title = version;
+        }
+        */
+
+     }
+
 }
+   
