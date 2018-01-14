9226d68b458014132735f3a6f8d919089dbc94f8
BrunoPereira <brunop.esac@hotmail.com>
2018-01-14 02:48:44 +0000

    Started Web Tests(Selenium)

---
+++ Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c5e983b-740e-4851-9295-b31174642969")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string baseURL = "cimob-ips.azurewebsites.net";
+        private IWebDriver driver;
+
+        [TestMethod]
+        public void LoginTest()
+        {
+            driver = new ChromeDriver(@"C:\chromeWebDriver\");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl(baseURL+"/Login");
+
+            IWebElement loginEmail = driver.FindElement(By.Id("Email"));
+            loginEmail.SendKeys("150221022@estudantes.ips.pt");
+
+            IWebElement loginPassword = driver.FindElement(By.Id("input-pw"));
+            loginPassword.SendKeys("teste");
+            loginPassword.SendKeys(Keys.Enter);
+        }
+    }
+}
