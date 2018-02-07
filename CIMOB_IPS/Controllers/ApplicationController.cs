a22171524b4d8826d912408235aa7fd5cbb1f12e
TheSevenDev <diogo_oliveira_9@hotmail.com>
2018-02-07 17:56:21 +0000

    - added more bread to the interview email text - started view of application and evaluation in mobilities in charge

--- CIMOB_IPS/Controllers/ApplicationController.cs
+++ CIMOB_IPS/Controllers/ApplicationController.cs
@@ -468,7 +468,6 @@ namespace CIMOB_IPS.Controllers
                 return RedirectToAction("MyApplications", "Application");
         }
         
-
         [HttpGet]
         public IActionResult Details(string id)
         {
@@ -497,6 +496,30 @@ namespace CIMOB_IPS.Controllers
                     application.IdProgramNavigation.IdProgramTypeNavigation = context.ProgramType.Where(p => p.IdProgramType == application.IdProgramNavigation.IdProgramType).SingleOrDefault();
 
                     return PartialView("_ApplicationDetails", application);
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult EvaluationDetails(string id)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Login", "Account");
+
+            if (!(User.IsInRole("tecnico") || User.IsInRole("tecnico_admin")))
+                return RedirectToAction("Index", "Home");
+
+            using (var context = new CIMOB_IPS_DBContext(new DbContextOptions<CIMOB_IPS_DBContext>()))
+            {
+                if (!context.Application.Where(a => a.IdApplication == int.Parse(id)).Any())
+                    return RedirectToAction("Index", "Application");
+                else
+                {
+                    var applicationEvaluation = context.ApplicationEvaluation.Where(ae => ae.IdApplication == int.Parse(id))
+                        .Include(ae => ae.IdApplication)
+                        .SingleOrDefault();
+
+                    return PartialView("_ApplicationEvaluationDetails", applicationEvaluation);
                 }
             }
         }
@@ -585,6 +608,9 @@ namespace CIMOB_IPS.Controllers
                     strbBody.AppendFormat(@"Informamos que a sua candidatura ao programa " + application.IdProgramNavigation.IdProgramTypeNavigation.Name);
                     strbBody.AppendFormat(" tem entrevista marcada para o dia " + interview.Date.Day + "/" + interview.Date.Month + "/" + interview.Date.Year);
                     strbBody.AppendFormat(" às " + interview.Date.Hour.ToString("D2") + ":" + interview.Date.Minute.ToString("D2") + ".<br>");
+
+                    strbBody.AppendFormat("Esta entrevista terá como principal objectivo a avaliação das condições para a realização da acção de mobilidade, ");
+                    strbBody.AppendFormat("designadamente o domínio da língua requerido pela instituição / entidade de acolhimento e o seu grau de autonomia demonstrado. <br>");
 
                     strbBody.AppendFormat("Caso não consiga comparecer, informe qualquer técnico disponível <a href=\"" + strLink + "\">nesta página</a> para uma remarcação.<br>");
 
@@ -813,6 +839,18 @@ namespace CIMOB_IPS.Controllers
             }
 
             return RedirectToAction("Index", "Application");
+        }
+
+        public void InsertEvaluation(ApplicationEvaluationViewModel viewModel)
+        {
+            using (var context = new CIMOB_IPS_DBContext(new DbContextOptions<CIMOB_IPS_DBContext>()))
+            {
+                ApplicationEvaluation appCancel = new ApplicationEvaluation
+                {
+                    /*IdApplication = viewModel.IdApplication,
+                    CreditsRatio = viewModel.*/
+                };
+            }
         }
 
         private IEnumerable<SelectListItem> PopulateTechnicians()
